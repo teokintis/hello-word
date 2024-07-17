4eb046e12ed85c54c9013104df5e3f305879b0d8
kinti <teo4asoee@gmail.com>
2024-07-17 16:23:53 +0300

    init

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,44 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <h1>Hello World</h1>
+    <button @click="fetchMessage">Get Message from Node.js API</button>
+    <p v-if="message">{{ message }}</p>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      message: '',
+    };
+  },
+  methods: {
+    async fetchMessage() {
+      try {
+        const response = await axios.get('http://52.57.9.0:3000/api');
+        this.message = response.data.message;
+      } catch (error) {
+        console.error('There was an error!', error);
+      }
+    },
+  },
+};
 </script>
 
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+button {
+  margin-top: 20px;
+  padding: 10px 20px;
+  font-size: 16px;
 }
 </style>
